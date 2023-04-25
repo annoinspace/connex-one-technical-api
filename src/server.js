e29524cd71b67ed71e0bc4ecf990452d79a04d01
annoinspace <aneesahalmask@gmail.com>
2023-04-25 10:00:58 +0100

    adding cors whitelist

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 // using listEndpoints as an additional package to view available endpoints
 import listEndpoints from "express-list-endpoints"
 import { forbiddenErrorHandler } from "./errorHandlers.js"
@@ -6,6 +7,22 @@ import timeRouter from "./api/time/index.js"
 
 const server = express()
 const port = 3001
+
+// ---------------- WHITELIST FOR CORS ------------------
+// adding the frontend url to cors to allow access to api
+const corsOptions = {
+  cors: {
+    origin: process.env.FE_URL,
+
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }
+}
+
+server.use(express.json())
+server.use(cors(corsOptions))
+
+// server.use(cors())
 
 // ---------------- ENDPOINTS ------------------
 //separating endpoints from the main server file so it's a little neater
